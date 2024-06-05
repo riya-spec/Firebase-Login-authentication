c91cc4c6601b03f669527c5e2a14e8bb2f99e7b3
Riya <71124402+riya-spec@users.noreply.github.com>
2024-06-05 16:35:44 +0530

    Update firebase.config.js

--- firebase.config.js
+++ firebase.config.js
@@ -3,9 +3,9 @@ import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   // Your Firebase project configuration details here
-  apiKey: "AIzaSyCe8LIVCjVrc8nXR5_5myCpUkgR7fBo3q0", // Replace with your actual API key
+  apiKey: "wjhdskjdhkjsjhdhskhsh", // Replace with your actual API key
   authDomain: "YOUR_FIREBASE_AUTH_DOMAIN.firebaseapp.com", // Replace with your auth domain
-  projectId: "login-e8f02.firebaseapp.com", // Replace with your project ID
+  projectId: "login234.firebaseapp.com", // Replace with your project ID
 };
 
 const app = initializeApp(firebaseConfig);
